e84994e24632fd319776b46093209f5cea99a123
ridho <ridho@gmail.com>
2026-01-26 02:02:54 +0800

    -

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -19,9 +19,7 @@ class UserMiddleware
     {
         $userAuth = Auth::user();
 
-        if (
-            $userAuth?->role !== UserRole::PATIENT
-        ) {
+        if ($userAuth?->role !== UserRole::PATIENT) {
             abort(403, 'Unauthorized action.');
         }
 
